fcc8b84dd5f6716880cffd85315e1c391fb4e857
mrdsx <168657227+mrdsx@users.noreply.github.com>
2025-04-05 17:41:33 +0300

    Delete singe-page-cv directory

--- singe-page-cv/index.js
+++
@@ -1,5 +0,0 @@
-const button = document.querySelector("button");
-button.addEventListener("click", () => {
-  const usernameInput = document.getElementById("username");
-  console.log(usernameInput.value);
-});
