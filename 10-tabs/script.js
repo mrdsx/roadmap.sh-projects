e83428f19dbbab460227922c1689a61e8a9ef106
mrdsx <andre0.castlegames@gmail.com>
2025-04-17 20:07:26 +0300

    added new project Tabs

---
+++ 10-tabs/script.js
@@ -0,0 +1,14 @@
+function openTab(event, tabId) {
+  const tabContens = document.querySelectorAll(".tab-content");
+  tabContens.forEach(tab => tab.style.display = "none");
+
+  const tabLinks = document.querySelectorAll(".tab-link");
+  tabLinks.forEach(tabLink => tabLink.classList.remove("active"));
+
+  document.getElementById(tabId).style.display = "block";
+  event.target.classList.add("active");
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('.tab-content').style.display = 'block';
+});
